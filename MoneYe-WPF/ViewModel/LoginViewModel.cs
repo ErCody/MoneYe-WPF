d35a896ca5193a2f927c3cf37899c4fabaea8050
ErCody <Tagi_ks46@itstep.edu.az>
2021-11-07 14:11:00 +0400

    Logic for Create New command

--- MoneYe-WPF/ViewModel/LoginViewModel.cs
+++ MoneYe-WPF/ViewModel/LoginViewModel.cs
@@ -1,5 +1,9 @@
-﻿using GalaSoft.MvvmLight;
+﻿using System;
+using System.Windows.Controls;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
 using GalaSoft.MvvmLight.Messaging;
+using MoneYe_WPF.Model;
 using MoneYe_WPF.Services;
 
 namespace MoneYe_WPF.ViewModel
@@ -8,11 +12,30 @@ namespace MoneYe_WPF.ViewModel
     {
         public INavigationService NavigationService;
         public IMessenger Messenger;
+        public IErrorLogger Logger;
 
-        public LoginViewModel(INavigationService navigationService, IMessenger messenger)
+        public LoginViewModel(INavigationService navigationService, IMessenger messenger, IErrorLogger logger)
         {
             NavigationService = navigationService;
             Messenger = messenger;
+            Logger = logger;
         }
+
+        private RelayCommand _regCommand;
+
+        public RelayCommand RegCommand => _regCommand ??= new RelayCommand(
+                () =>
+                {
+                    try
+                    {
+                        
+                        NavigationService.NavigateTo<RegViewModel>();
+                    }
+                    catch (Exception e)
+                    {
+                        Logger.LogError("Unhandled error on RegCommand", e);
+                    }
+                }
+            );
     }
 }
